927de822a9385c4f4a164b9afb64350a3ce34183
Tatsuya Hanyu <s1210099@gmail.com>
2017-05-10 17:52:39 +0900

    deleted some ruby files

--- count_eval.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
-# vim:set fileencoding=utf-8:
-
-ARGF.each do |line|
-    line.chomp!
-    parity = line.split(",")
-    print "#{(parity.count("1")+1)%2},"
-end
